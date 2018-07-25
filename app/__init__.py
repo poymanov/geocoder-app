f38268f3086df0b1b20f70b83ecfa19b6a244018
Nicolay Poymanov <n.poymanov@gmail.com>
2018-07-25 21:15:37 +0300

    Add csv file data modification

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import Config
+from geopy.geocoders import Nominatim
+import pandas
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -10,5 +12,25 @@ Bootstrap(app)
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() == 'csv'
+
+def get_geo_data(address):
+	nom = Nominatim(user_agent="geocoder-app")
+	return nom.geocode(address)
+	
+def create_pandas_df(file):
+	df = pandas.read_csv(file)
+
+	if 'Address' in df.columns:
+		address_column = 'Address'
+	elif 'address' in df.columns:
+		address_column = 'address'
+	else:	
+		return None
+
+	df['Coordinates'] = df[address_column].apply(get_geo_data)
+	df['Latitude'] = df['Coordinates'].apply(lambda x: x.latitude if x != None else None)			
+	df['Longitude'] = df['Coordinates'].apply(lambda x: x.longitude if x != None else None)
+
+	return df
 
 from app import routes, forms
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, redirect, request, url_for
-from app import app, allowed_file
+from app import app, allowed_file, create_pandas_df
 from app.forms import UploadForm
 from werkzeug import secure_filename
 import os
@@ -18,10 +18,16 @@ def process():
 		filename = secure_filename(file.filename)
 
 		if file and allowed_file(filename):
-			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))	
-			return redirect(url_for('home'))
+			df = create_pandas_df(file)
+
+			if df is None:
+				form.upload_file.errors.append("Your file is missing a column 'Address' or 'address'")
+				return render_template('home.html', form=form)		
+			else:
+				df.to_csv(os.path.join(app.config['UPLOAD_FOLDER'], filename), encoding='utf-8', index=False)
+				return redirect(url_for('home'))
 		else:
-			form.upload_file.errors.append('You must upload only .csv files')
+			form.upload_file.errors.append("You must upload only .csv files")
 			return render_template('home.html', form=form)	
 
 	else:
